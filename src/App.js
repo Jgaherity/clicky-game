346b9e089f37db5a26d66e1df5ff83697df9d4db
Jackie <jagaheri@microsoft.com>
2019-01-08 16:45:51 -0800

    initial work

--- src/App.js
+++ src/App.js
@@ -1,28 +1,126 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import Navbar from "./components/Navbar";
+// import Jumbotron from "./components/Jumbotron";
+// import FriendCard from "./components/FriendCard";
+// import Footer from "./components/Footer";
+// import fish from "./fish.json";
+// import './App.css';
 
+
+
+// //root level file. 
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+
+//imports dependencies and files
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
+import FriendCard from "./components/FriendCard";
+import Footer from "./components/Footer";
+import character from "./characters.json";
+import "./App.css";
+
+//sets state to 0 or empty
 class App extends Component {
+  state = {
+    character,
+    clickedCharacter: [],
+    score: 0
+  };
+
+//when you click on a card ... the fish is taken out of the array
+  imageClick = event => {
+    const currentCharacter = event.target.alt;
+    const CharacterAlreadyClicked =
+      this.state.clickedCharacter.indexOf(currentCharacter) > -1;
+
+//if you click on a fish that has already been selected, the game is reset and cards reordered
+    if (CharacterAlreadyClicked) {
+      this.setState({
+        character: this.state.character.sort(function(a, b) {
+          return 0.5 - Math.random();
+        }),
+        clickedCharacter: [],
+        score: 0
+      });
+        alert("You lose. Play again?");
+
+//if you click on an available fish, your score is increased and cards reordered
+    } else {
+      this.setState(
+        {
+          character: this.state.character.sort(function(a, b) {
+            return 0.5 - Math.random();
+          }),
+          clickedCharacter: this.state.clickedCharacter.concat(
+            currentCharacter
+          ),
+          score: this.state.score + 1
+        },
+//if you get all 12 fish corrent you get a congrats message and the game resets        
+        () => {
+          if (this.state.score === 12) {
+            alert("Yay! You Win!");
+            this.setState({
+              character: this.state.character.sort(function(a, b) {
+                return 0.5 - Math.random();
+              }),
+              clickedCharacter: [],
+              score: 0
+            });
+          }
+        }
+      );
+    }
+  };
+
+//the order of components to be rendered: navbar, jumbotron, friendcard, footer 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navbar 
+          score={this.state.score}
+        />
+        <Jumbotron />
+        <div className="wrapper">
+          {this.state.character.map(character => (
+            <FriendCard
+              imageClick={this.imageClick}
+              id={character.id}
+              key={character.id}
+              image={character.image}
+            />
+          ))}
+        </div>
+        <Footer />
       </div>
     );
   }
 }
-
 export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+//Footer component
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+	<header className = "footer">
+		<p>I hope you like Game of Thrones!</p>
+	</header>
+);
+
+export default Footer;
