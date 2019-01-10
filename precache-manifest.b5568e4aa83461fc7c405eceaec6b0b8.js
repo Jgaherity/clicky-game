f28f2d8ab260cd44f67d265807d2a45438fc5c82
Jackie <jagaheri@microsoft.com>
2019-01-09 16:39:16 -0800

    Updates

---
+++ precache-manifest.b5568e4aa83461fc7c405eceaec6b0b8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "52307e2ea37177fa9b7b",
+    "url": "/clicky-game/static/css/main.e9ea3ce7.chunk.css"
+  },
+  {
+    "revision": "52307e2ea37177fa9b7b",
+    "url": "/clicky-game/static/js/main.52307e2e.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/clicky-game/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "1e889c74c76ec7ef05712cbbc9769206",
+    "url": "/clicky-game/static/media/Arya_Stark.1e889c74.jpeg"
+  },
+  {
+    "revision": "4e4e2da731e7f6837e2e20fd78a6ae5d",
+    "url": "/clicky-game/static/media/Cersei_Lannister.4e4e2da7.jpeg"
+  },
+  {
+    "revision": "cabbe439a51a8d525bead485dc9f5ccb",
+    "url": "/clicky-game/static/media/Daenerys_Targaryen.cabbe439.jpeg"
+  },
+  {
+    "revision": "cebe11cdeba75e63331548323282d93a",
+    "url": "/clicky-game/static/media/Jaime_Lannister.cebe11cd.jpeg"
+  },
+  {
+    "revision": "ccf9468efa0d2b139ef8bbadb43da3b8",
+    "url": "/clicky-game/static/media/Joffrey_Baratheon.ccf9468e.jpeg"
+  },
+  {
+    "revision": "2ac6e35aaa3496c17f6953c7d052a9fa",
+    "url": "/clicky-game/static/media/Jon_Snow.2ac6e35a.jpeg"
+  },
+  {
+    "revision": "17bd4ebdc651d439340efcf6ee75a37e",
+    "url": "/clicky-game/static/media/Littlefinger.17bd4ebd.jpeg"
+  },
+  {
+    "revision": "6d86f2f62edbfb6f265515ec5bd5b60f",
+    "url": "/clicky-game/static/media/Robb_Stark.6d86f2f6.jpeg"
+  },
+  {
+    "revision": "7f0e96f962002433080d34b8d4eb7db1",
+    "url": "/clicky-game/static/media/Sansa_Stark.7f0e96f9.jpeg"
+  },
+  {
+    "revision": "37c2788966fa4f40e182598bec9ffa3b",
+    "url": "/clicky-game/static/media/Theon_GreyJoy.37c27889.jpeg"
+  },
+  {
+    "revision": "0d5a95e5de7caf725fe02bed0560c25f",
+    "url": "/clicky-game/static/media/Tyrion_Lannister.0d5a95e5.jpeg"
+  },
+  {
+    "revision": "957da669e5d982aa6cf880730b3f174c",
+    "url": "/clicky-game/static/media/catelyn_stark.957da669.jpeg"
+  },
+  {
+    "revision": "a03c943b0c845f97aa7b0733b1597a28",
+    "url": "/clicky-game/static/media/ned_stark.a03c943b.jpeg"
+  },
+  {
+    "revision": "9aa13edec3b356b82765d7a04dcc1ecd",
+    "url": "/clicky-game/static/media/robert_baratheon.9aa13ede.jpeg"
+  },
+  {
+    "revision": "35474bf7c46c2b11d7b8327b1d7d9e1b",
+    "url": "/clicky-game/index.html"
+  }
+];
